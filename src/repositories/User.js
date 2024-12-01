974c8197991eac4e3ad3e9da37e3a6533b8c9340
luccasPhilot <luccasgoncalvrs@gmail.com>
2024-12-01 14:43:23 -0300

    Modificando Usuarios

--- src/repositories/User.js
+++ src/repositories/User.js
@@ -1,5 +1,44 @@
-const User = require('../models/User');
+const fs = require('fs');
+const path = require('path');
 
-exports.findByUsername = async (username) => {
-  return await User.findOne({ where: { username } });
+// Caminho para o arquivo JSON onde os dados serão armazenados
+const filePath = path.join(__dirname, '..', '..', 'DataSource', 'User.json');
+
+// Função para ler o arquivo JSON
+const readFile = () => {
+  const data = fs.readFileSync(filePath, 'utf8');
+  return JSON.parse(data);
 };
+
+// Função para escrever no arquivo JSON
+const writeFile = (data) => {
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+};
+
+const findByUsername = async (username) => {
+  const users = readFile();
+  return users.find((user) => user.username === username) || null;
+};
+
+const saveUser = async (user) => {
+  const users = readFile();
+  users.push(user);
+  writeFile(users);
+};
+
+const updateUser = async (username, updatedData) => {
+  const users = readFile();
+  const index = users.findIndex((user) => user.username === username);
+  if (index !== -1) {
+    users[index] = { ...users[index], ...updatedData };
+    writeFile(users);
+    return users[index];
+  }
+  return null;
+};
+
+module.exports = {
+  findByUsername,
+  saveUser,
+  updateUser
+}
